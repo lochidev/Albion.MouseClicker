1d7337cfb015127cf3da9d2cc08ec99b45fdee37
lochi <72388533+lochidev@users.noreply.github.com>
2023-03-26 15:13:50 +0530

    remove unused code

--- Albion.MouseClicker/Albion.MouseClicker.cpp
+++ Albion.MouseClicker/Albion.MouseClicker.cpp
@@ -2,40 +2,8 @@
 #include <thread>
 #include <chrono>
 #include "AlWin.h"
-void ShowDesktop()
-{
-    while(true)
-    {
-
-        std::cout << "Sending 'Win-D'\n";
-        INPUT inputs[4] = {};
-        ZeroMemory(inputs, sizeof(inputs));
-
-        inputs[0].type = INPUT_KEYBOARD;
-        inputs[0].ki.wVk = VK_LWIN;
-
-        inputs[1].type = INPUT_KEYBOARD;
-        inputs[1].ki.wVk = 0x44;
-
-        inputs[2].type = INPUT_KEYBOARD;
-        inputs[2].ki.wVk = 0x44;
-        inputs[2].ki.dwFlags = KEYEVENTF_KEYUP;
-
-        inputs[3].type = INPUT_KEYBOARD;
-        inputs[3].ki.wVk = VK_LWIN;
-        inputs[3].ki.dwFlags = KEYEVENTF_KEYUP;
-
-        UINT uSent = SendInput(ARRAYSIZE(inputs), inputs, sizeof(INPUT));
-        if (uSent != ARRAYSIZE(inputs))
-        {
-            std::cout << "SendInput failed: 0x%x\n" << HRESULT_FROM_WIN32(GetLastError());
-        }
-        std::cout << "Sent!";
-    }
-}
 int main()
 {
-    //ShowDesktop();
     while (true)
     {
         using namespace std::chrono_literals;
